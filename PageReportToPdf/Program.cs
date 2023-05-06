d8405409ae6da2bb1109d641577a2d99379c1fd9
Yuuichi <Yuuichi@WIN11TEST>
2023-05-06 22:35:36 +0900

    プロジェクト ファイルを追加します。

---
+++ PageReportToPdf/Program.cs
@@ -0,0 +1,27 @@
+﻿// レポートファイルがあるパス
+var reportPath = "PageReportToPdf.Reports.Estimate_page_ipa.rdlx";
+
+// 埋め込みリソースを Stream をして取り出す
+var stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(reportPath);
+if (stream == null) return;
+
+// ページレポートに変換します
+using var sr = new StreamReader(stream);
+var pageReport = new GrapeCity.ActiveReports.PageReport(sr);
+
+// PDF 描画拡張を定義します
+var pdfRenderingExtension = new GrapeCity.ActiveReports.Export.Pdf.Page.PdfRenderingExtension();
+
+// メモリストリームとして出力する定義です
+var outputProvider = new GrapeCity.ActiveReports.Rendering.IO.MemoryStreamProvider();
+
+// レポートを PDF にレンダリングします
+pageReport.Document.Render(pdfRenderingExtension, outputProvider);
+
+// Stream に変換します
+var ps = outputProvider.GetPrimaryStream();
+var outputStream = ps.OpenStream();
+
+// ファイルにストリームを書き出します
+using var fs = new FileStream("Output.pdf", FileMode.Create);
+outputStream.CopyTo(fs);
---
+++ SectionReportToPdf/Program.cs
@@ -0,0 +1,26 @@
+﻿// レポートファイルがあるパス
+var reportPath = "SectionReportToPdf.Reports.Invoice.rpx";
+
+// 埋め込みリソースを Stream をして取り出す
+var stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(reportPath);
+if (stream == null) return;
+
+// Stream を XML として読み込めるようにします
+var xr = System.Xml.XmlReader.Create(stream);
+
+// セクションレポートとして読み込みます
+var sectionReport = new GrapeCity.ActiveReports.SectionReport();
+sectionReport.LoadLayout(xr);
+sectionReport.Run();
+
+// PDF のエクスポートフィルターを作成します
+var pdfExport = new GrapeCity.ActiveReports.Export.Pdf.Section.PdfExport();
+
+// メモリストリームに PDF の情報を書き出します
+var outputStream = new System.IO.MemoryStream();
+pdfExport.Export(sectionReport.Document, outputStream);
+outputStream.Seek(0, SeekOrigin.Begin);
+
+// ファイルにストリームを書き出します
+using var fs = new FileStream("Output.pdf", FileMode.Create);
+outputStream.CopyTo(fs);
